bc7b8477043bb44632830b8def09deab853f685f
César Enrique Ramírez <caenrique93@gmail.com>
2019-09-04 12:22:33 +0200

    added new cli

--- src/main/scala/com/uhu/cesar/tetris/app/MainTraining.scala
+++ src/main/scala/com/uhu/cesar/tetris/app/MainTraining.scala
@@ -1,16 +1,19 @@
 package com.uhu.cesar.tetris.app
 
 import com.uhu.cesar.tetris.QFunction.{QFunctionLoader, QFunctionSerializer}
-import com.uhu.cesar.tetris.{QFunction, QPlayer}
+import com.uhu.cesar.tetris.QPlayer
 import sun.misc.Signal
 
 object MainTraining extends App with QFunctionLoader with QFunctionSerializer {
 
-  val qfunctionOption = if (args.nonEmpty) Some(loadQFunction(args(0))) else None
+  val options = CLIParser(args)
+  val qfunctionOption = options.get(CLIParser.QFUNCTION).asInstanceOf[Option[String]].map(s => loadQFunction(s))
+  val episodes = options.get(CLIParser.EPISODES).asInstanceOf[Option[Int]]
+
   val player: QPlayer = QPlayer(training = true, qfunctionOption)
 
   Signal.handle(new Signal("INT"), _ => {
-    player.writeQFunction
+    player.writeQFunction()
     System.exit(0)
   })
 
